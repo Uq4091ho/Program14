1ce075687d62a3c46a9140d8604470e16d7faf72
Uq4091ho <lidenberja@mnstate.edu>
2016-04-22 12:45:40 -0500

    Update modSet.py

--- modSet.py
+++ modSet.py
@@ -7,3 +7,15 @@ __date__="4/22/2016"
 
 
 class CustomSet:
+
+   def __str__(self):
+        """
+        Description: returns a string, Coded by  Jared Lidenberg
+        Pre-Conditions: none
+        Post-Conditions: converts attributes into str's for printing
+        """
+        tmp = "{"
+        for el in self._setElements:
+            tmp += el + ","
+        tmp += "}"
+        return tmp
