0a2010b999a0b92db1eb02f77a390d2210591bad
Uq4091ho <lidenberja@mnstate.edu>
2016-04-27 12:49:53 -0500

    Update modSet.py

--- modSet.py
+++ modSet.py
@@ -19,3 +19,29 @@ class CustomSet:
             tmp += el + ","
         tmp += "}"
         return tmp
+        
+   def __le__(self, secList):
+        """
+        Description: Checks for a subset, Coded by  Jared Lidenberg.
+        Pre-Conditions: self and secList must be sets
+        Post-Conditions: none
+        """
+        s1 = self
+        s2 = secList
+        for ele in s1:
+            if not ele in s2:
+                return False
+        return True
+        
+   def __ge__(self, secList): #set 7
+        """
+        Description: Checks for superset, Coded by  Jared Lidenberg.
+        Pre-Conditions: self and secList must be sets
+        Post-Conditions: none
+        """
+        s1 = self
+        s2 = secList
+        for ele in s2:
+            if not ele in s1:
+                return False
+        return True
